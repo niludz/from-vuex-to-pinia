daaeacfd20a4db338081406cb5ba288ffdd2de9b
rjwang <rjwang@zjft.com>
2024-02-26 20:25:58 +0800

    action

--- src/stores/EventStore.js
+++ src/stores/EventStore.js
@@ -1,11 +1,24 @@
 import { defineStore } from "pinia";
+import EventService from '../services/EventService.js'
+
 
 export const useEventStore = defineStore('EventStore', {
-    state()=> ({
+    state: () => ({
         events: [],
         event: {}
     }),
-        getters: {
-    numberOfEvents: state => state.events.length
-}
+    getters: {
+        numberOfEvents: state => state.events.length
+    },
+    actions: {
+        fetchEvents() {
+            return EventService.getEvents()
+                .then(response => {
+                    this.events = response.data
+                })
+                .catch(error => {
+                    throw error
+                })
+        },
+    }
 })
